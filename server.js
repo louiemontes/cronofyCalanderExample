58eb8b0de6ea08e3a6e17595c44f5ada831ae3fd
Luis Montes <lmontes@bolste.com>
2018-12-26 13:12:05 -0700

    risky

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
+const axios = require("axios");
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const axios = require("axios");
+const btoa = require("btoa");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.get("/cat", function(req, res) {
-  console.log(__dirname);
-
   axios
     .get("http://thecatapi.com/api/images/get")
     .then(data => res.send(data.request.res.responseUrl))
     .catch(err => console.log(err));
 });
 
-// Anything that doesn't match the above, send back index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+// potential replacement - need to handle response correctly...
+
+// app.get("/cat", function(req, res) {
+//   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+//   axios
+//     .get("https://cataas.com/cat")
+//     .then(data => {
+//       res.send(data.data) // type of response: image/jpeg potentially a buffer...
+//     })
+//     .catch(err => console.log(err));
+// });
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
